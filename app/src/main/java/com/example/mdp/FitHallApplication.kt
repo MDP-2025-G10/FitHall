116f50dc1413e8fc58d82745d52da2585771bf5c
fuzzykala <tangerinekey381@gmail.com>
2025-03-15 16:37:28 +0200

    delete testing code in mainActivity

--- app/src/main/java/com/example/mdp/FitHallApplication.kt
+++ app/src/main/java/com/example/mdp/FitHallApplication.kt
@@ -2,13 +2,14 @@ package com.example.mdp
 
 import android.app.Application
 import com.example.mdp.di.appModule
+import com.google.firebase.FirebaseApp
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
 class FitHallApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-
+        FirebaseApp.initializeApp(this)
         startKoin {
             // Android context
             androidContext(this@FitHallApplication)
