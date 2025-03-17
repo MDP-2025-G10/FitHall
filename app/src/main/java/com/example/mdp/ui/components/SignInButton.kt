5ae2de1c687dabc120d2dc35f773b753bb321c03
fuzzykala <tangerinekey381@gmail.com>
2025-03-17 17:35:45 +0200

    remove unnecessary import

--- app/src/main/java/com/example/mdp/ui/components/SignInButton.kt
+++ app/src/main/java/com/example/mdp/ui/components/SignInButton.kt
@@ -10,14 +10,11 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
