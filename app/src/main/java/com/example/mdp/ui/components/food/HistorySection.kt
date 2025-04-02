b5f68b709ebf839271a45638d7bf028c654317d5
fuzzykala <tangerinekey381@gmail.com>
2025-04-02 18:11:37 +0300

    optimize the history meal UI to show percentage of nutrition.

--- app/src/main/java/com/example/mdp/ui/components/food/HistorySection.kt
+++ app/src/main/java/com/example/mdp/ui/components/food/HistorySection.kt
@@ -1,5 +1,6 @@
 package com.example.mdp.ui.components.food
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -9,6 +10,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AddCircle
 import androidx.compose.material.icons.filled.RemoveCircle
@@ -25,9 +27,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import coil3.compose.AsyncImage
 import com.example.mdp.data.model.Meal
 import com.example.mdp.navigation.LocalDateViewModel
 import com.example.mdp.navigation.LocalMealViewModel
@@ -77,6 +82,7 @@ fun HistoryCard(meal: Meal) {
     val carbs = meal.carbs
     val protein = meal.proteins
     val fats = meal.fats
+    val imagePath = meal.imagePath
     val formattedDate = historyCardTimeFormatter(meal.timestamp)
 
     Card(
@@ -90,27 +96,42 @@ fun HistoryCard(meal: Meal) {
             Column(modifier = Modifier.weight(0.9f)) {
                 Text(mealName, style = MaterialTheme.typography.titleMedium)
                 Text("Calories: $calories kcal")
-                Text("Carbs: $carbs g")
-                Text("Protein: $protein g")
-                Text("Fats: $fats g")
-                Text("Time: $formattedDate")
-            }
-            Column(modifier = Modifier.weight(0.1f)) {
-                IconButton(onClick = { showPopup = true }) {
-                    Icon(
-                        imageVector = Icons.Filled.AddCircle,
-                        contentDescription = "add meal from history",
-                        tint = Color(0xFF5A67B4),
-                        modifier = Modifier.size(36.dp)
-                    )
-                }
-                IconButton(onClick = { mealViewModel.deleteMeal(meal) }) {
-                    Icon(
-                        imageVector = Icons.Filled.RemoveCircle,
-                        contentDescription = "Remove history",
-                        tint = Color(0xFFFC2D00),
-                        modifier = Modifier.size(36.dp)
-                    )
+//                Text("Carbs: $carbs g")
+//                Text("Protein: $protein g")
+//                Text("Fats: $fats g")
+                MacroProgressBar(carbs, protein, fats)
+                AsyncImage(
+                    model = imagePath,
+                    contentDescription = "Selected Image",
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clip(RoundedCornerShape(8.dp))
+                        .border(1.dp, Color.Gray, RoundedCornerShape(8.dp))
+                        .align(Alignment.CenterHorizontally)
+                        .size(200.dp),
+                    contentScale = ContentScale.Crop
+                )
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.padding(vertical = 5.dp)
+                ) {
+                    Text(formattedDate)
+                    IconButton(onClick = { showPopup = true }) {
+                        Icon(
+                            imageVector = Icons.Filled.AddCircle,
+                            contentDescription = "add meal from history",
+                            tint = Color(0xFF5A67B4),
+                            modifier = Modifier.size(30.dp)
+                        )
+                    }
+                    IconButton(onClick = { mealViewModel.deleteMeal(meal) }) {
+                        Icon(
+                            imageVector = Icons.Filled.RemoveCircle,
+                            contentDescription = "Remove history",
+                            tint = Color(0xFFAA5559),
+                            modifier = Modifier.size(30.dp)
+                        )
+                    }
                 }
             }
         }
