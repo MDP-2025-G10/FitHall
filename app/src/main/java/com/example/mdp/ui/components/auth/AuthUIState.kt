30398c97d491fd3025c53c388a41226a00e870a3
hduy0407 <t3ngdu01@students.oamk.fi>
2026-02-22 22:40:23 +0700

    unused class

--- app/src/main/java/com/example/mdp/ui/components/auth/AuthUIState.kt
+++ app/src/main/java/com/example/mdp/ui/components/auth/AuthUIState.kt
@@ -1,8 +1,9 @@
 package com.example.mdp.ui.components.auth
-
+/**
 sealed class AuthUiState {
     object Idle : AuthUiState()
     object Loading : AuthUiState()
     data class Success(val uid: String) : AuthUiState()
     data class Error(val message: String) : AuthUiState()
 }
+**/
