c1a8d814208286964c38f5493ffc303d45c3b690
hduy0407 <t3ngdu01@students.oamk.fi>
2026-02-22 22:37:45 +0700

    delete unused import

--- app/src/main/java/com/example/mdp/ui/screens/Auth.kt
+++ app/src/main/java/com/example/mdp/ui/screens/Auth.kt
@@ -22,14 +22,12 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import com.example.mdp.firebase.auth.viewModel.AuthViewModel
 import com.example.mdp.navigation.LocalAuthViewModel
 import com.example.mdp.navigation.NavRoutes
 import com.example.mdp.ui.components.auth.AppLogo
 import com.example.mdp.ui.components.auth.AuthButton
 import com.example.mdp.ui.components.auth.AuthInput
 import com.example.mdp.ui.components.auth.SignInButton
-import org.koin.androidx.compose.koinViewModel
 
 @RequiresApi(Build.VERSION_CODES.UPSIDE_DOWN_CAKE)
 @Composable
